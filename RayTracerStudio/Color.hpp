ab97e72ed32908398f464663844c784961aeff79
zmfrazier <zmf9@zips.uakron.edu>
2017-02-17 20:36:14 -0500

    Added constructors to color class

--- RayTracerStudio/Color.hpp
+++ RayTracerStudio/Color.hpp
@@ -3,10 +3,11 @@
 
 class Color {
 public:
-   double r;
-   double g;
-   double b;
-   double a; 
+  double r, g, b, a;
+
+  Color() : r(0.0), g(0.0), b(0.0), a(1.0) {}
+  Color(double r, double g, double b) : r(r), g(g), b(b), a(1.0) {}
+  Color(double r, double g, double b, double a) : r(r), g(g), b(b), a(a) {}
 };
 
 #endif
