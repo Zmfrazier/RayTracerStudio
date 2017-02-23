50d257a2817be07c5ca502d23c9560111cbc3f4e
zmfrazier <zmf9@zips.uakron.edu>
2017-02-23 09:57:33 -0500

    Included calculateWUV method as well as a initialization list for camera class

--- RayTracerStudio/Camera.cpp
+++ RayTracerStudio/Camera.cpp
@@ -1,1 +1,7 @@
 #include "Camera.hpp"
+
+void Camera::calculateWUV(){
+  w = (lookAt - position).normalize();
+  u = up.cross(w).normalize();
+  v = w.cross(u);
+}
--- RayTracerStudio/Camera.hpp
+++ RayTracerStudio/Camera.hpp
@@ -6,10 +6,34 @@
 class Camera {
 public:
   Vector position;
+  Vector w, u, v;
+  Vector up;
+  Vector pointedAt;
+  double screenWidth;
 
   Camera() {
     position = Vector(0.0, 0.0, 100.0);
+    up = Vector(0.0, 1.0, 0.0);
+    lookAt = Vector(0.0, 0.0, 0.0);
+    screenWidth = 1000;
+
+    calculateWUV();
   }
+
+  Camera(
+    Vector p, 
+    Vector u,
+    Vector l, 
+    double s
+    ) 
+    :
+    position(p), 
+    up(u), 
+    lookAt(l), 
+    screenWidth(s) 
+    {calculateWUV();}
+
+  void calculateWUV();
 };
 
 #endif
