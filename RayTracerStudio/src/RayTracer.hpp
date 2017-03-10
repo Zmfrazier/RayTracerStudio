ec6aaf411526e7f0f51e6efdb8672881211fb841
zmfrazier <zmf9@zips.uakron.edu>
2017-03-10 17:01:44 -0500

    Update make file

--- RayTracerStudio/src/RayTracer.hpp
+++ RayTracerStudio/src/RayTracer.hpp
@@ -1,9 +1,11 @@
 #ifndef __RAY_TRACER_HPP__
 #define __RAY_TRACER_HPP__
 #include <vector>
+#include "Camera.hpp"
 
 class Object;
 class Light;
+class Camera;
 
 class RayTracer{
 public:
--- RayTracerStudio/src/main.cpp
+++ RayTracerStudio/src/main.cpp
@@ -1,10 +1,3 @@
-#include "Camera.cpp"
-#include "Color.cpp"
-#include "Object.cpp"
-#include "Ray.cpp"
-#include "RayTracer.cpp"
-#include "Vector.cpp"
-
 
 int main(){
 	
