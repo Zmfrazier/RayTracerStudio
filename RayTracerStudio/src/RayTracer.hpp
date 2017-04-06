2753e17c5a166d4ace9bd673b674f7aa9b0a05f2
zmfrazier <zmf9@zips.uakron.edu>
2017-04-06 09:41:48 -0400

    Included functions for ray tracer class that cast pixels at certain points

--- RayTracerStudio/src/RayTracer.hpp
+++ RayTracerStudio/src/RayTracer.hpp
@@ -1,32 +1,53 @@
-#ifndef __RAY_TRACER_HPP__
-#define __RAY_TRACER_HPP__
-#include <vector>
-#include "Camera.hpp"
+#ifndef __RAY_TRACER_H__
+#define __RAY_TRACER_H__
 
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include "Vector.h"
+#include "Camera.h"
+
+class Ray;
+class Color;
+class Intersection;
 class Object;
 class Light;
-class Camera;
 
-class RayTracer{
+class RayTracer {
 public:
-  int width;
-  int height;
-  int maxReflections;
-  
-  Camera camera;
+   int width;
+   int height;
+   int maxReflections;
+   int superSamples; // Square root of number of samples to use for each pixel.
+   Camera camera;
+   double imageScaling;
+   int depthComplexity;
+   unsigned long long raysCast;
+   BSP* bsp;
 
-  std::vector<Object*> objects;
-  std::vector<Light*> lights;
+   std::vector<Object*> objects;
+   std::vector<Light*> lights;
+   std::map<std::string, Material*> materials;
 
-  void
-  addObject(Object*);
+   RayTracer(int, int, int, int, int);
 
-  void
-  addLight(Light*);
+   ~RayTracer();
 
-  RayTracer();
-  ~RayTracer();
+   void addObject(Object* object) {
+      objects.push_back(object);
+   }
 
+   void addLight(Light* light) {
+      lights.push_back(light);
+   }
+
+   void traceRays(std::string);
+
+private:
+   Color castRayForPixel(int, int);
+   Color castRayAtPoint(const Vector&);
 };
 
 #endif
