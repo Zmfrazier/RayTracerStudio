13392266730d24ea58a11a47d1bc26e904ea2886
zmfrazier <zmf9@zips.uakron.edu>
2017-05-09 11:33:03 -0400

    Removed old main.cpp, updated cmakelists.txt, and fixed naming error for Primitives.cpp

--- RayTracerStudio/src/Primitives.cpp
+++ RayTracerStudio/src/Primitives.cpp
@@ -1,101 +1,67 @@
-#ifndef __PRIMITIVES_HPP
-#define __PRIMITIVES_HPP
-#include <vector>
-
-struct Point {
-	float x, y, z;
-};
-
-struct Color {
-    float red, green, blue;
-};
-
-struct Vector {
-	float x, y, z;
-};
-
-struct Material {
-    float reflection;
-	float red, green, blue;
-	float power;
-	float specular;
-};
-
-struct Sphere {
-	Point pos;
-	float size;
-	int materialId;
-};
-
-struct Plane {
-    Point pos;
-    Vector normal;
-    int materialId;
-};
-
-struct Light {
-	Point pos;
-	Color intensity;
-};
-
-struct Ray {
-	Point start;
-	Vector dir;
-};
-
-struct Camera{
-    float Horizontal;
-    float Vertical;
-    float Depth;
-    Vector Rotate;
-};
-
-struct Scene {
-    std::vector<Material> Materials;
-    std::vector<Sphere>   Spheres;
-    std::vector<Plane>    Planes;
-    std::vector<Light>    Lights;
-
-    int width, height;
-
-    Camera Cam;
-};
-
-bool Initialize(char* InputName, Scene &MyScene);
-
-#define invsqrtf(x) ((1.0f) / sqrtf(x))
+#include "Primitives.hpp"
 
 Point 
-operator + (const Point& p, const Vector& v);
+operator + (const Point& p, const Vector& v){
+    Point pt = {p.x + v.x, p.y + v.y, p.z + v.z };
+    return pt;
+}
 
 Point 
-operator - (const Point& p, const Vector& v);
+operator - (const Point& p, const Vector& v){
+    Point pt = {p.x - v.x, p.y - v.y, p.z - v.z };
+    return pt;
+}
 
 Vector 
-operator + (const Vector& v1, const Vector& v2);
+operator + (const Vector& v1, const Vector& v2){
+    Vector vec = {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z };
+    return vec;
+}
 
 Vector 
-operator - (const Point& p1, const Point& p2);
+operator - (const Point& p1, const Point& p2){
+    Vector v = {p1.x - p2.x, p1.y - p2.y, p1.z - p2.z };
+    return v;
+}
 
 Vector 
-operator * (float c, const Vector& v);
+operator * (float c, const Vector& v)
+{
+    Vector vec = {v.x * c, v.y * c, v.z * c};
+    return vec;
+}
 
 Vector 
-operator - (const Vector& v1, const Vector& v2);
+operator - (const Vector& v1, const Vector& v2){
+    Vector vec = {v1.x - v2.x, v1.y - v2.y, v1.z - v2.z };
+    return vec;
+}
 
 float 
-operator * (const Vector& v1, const Vector& v2 );
+operator * (const Vector& v1, const Vector& v2 ) {
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
 
 Vector 
-operator ^ (const Vector& v1, const Vector& v2);
+operator ^ (const Vector& v1, const Vector& v2) {
+    Vector vec = {v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x};
+    return vec;
+}
 
 Color 
-operator * (const Color& c1, const Color& c2 );
+operator * (const Color& c1, const Color& c2) {
+    Color c = {c1.red * c2.red, c1.green * c2.green, c1.blue * c2.blue};
+    return c;
+}
 
 Color 
-operator + (const Color& c1, const Color& c2 );
+operator + (const Color&c1, const Color &c2) {
+    Color c = {c1.red + c2.red, c1.green + c2.green, c1.blue + c2.blue};
+    return c;
+}
 
 Color 
-operator * (float coefficient, const Color& c );
-
-#endif 
+operator * (float coefficient, const Color &c) {
+    Color c2 = {c.red * coefficient, c.green * coefficient, c.blue * coefficient};
+    return c2;
+}
